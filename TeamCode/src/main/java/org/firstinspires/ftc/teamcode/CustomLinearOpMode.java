7482b9a5be7ab5dd5dfb44f8a9de9d4d66ed39f5
Summer Ely <summerely@gmail.com>
2019-09-12 08:40:15 -0500

    Created PID & IMU classes, added to OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomLinearOpMode.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class CustomLinearOpMode extends LinearOpMode
+{
+
+    // AUTO
+
+    // set motor names
+
+    public void runOpMode()
+    {
+
+    }
+
+    public void initialize()
+    {
+
+    }
+
+    public void waitForStart()
+    {
+        // if start is pressed
+            // runopmode
+        //else
+            //wait for start
+    }
+
+    //public void
+
+}
