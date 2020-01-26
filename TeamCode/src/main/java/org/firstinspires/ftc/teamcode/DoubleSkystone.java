90410bf008a639d3e56c7b5affbd8119a9a6be98
Summer Ely <summerely@gmail.com>
2020-01-26 15:40:08 -0600

    updated DoubleSkystone() to be centered on the 5th stone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleSkystone.java
@@ -7,23 +7,21 @@ import com.qualcomm.robotcore.util.RobotLog;
 @Autonomous(name = "DoubleSkystone", group = "6209")
 public class DoubleSkystone extends BaseLinearOpMode
 {
-
         boolean configOnly = false;
-        boolean isStartingBlue = true;
+        boolean isBlue = true;
         boolean parkOnSide = true;
 
         // TODO: test these distances
         // TODO: change this to work with the new mechanism
 
         private static int MOVE_OFF_OF_WALL_DISTANCE = 2;
-        private static int STRAFE_TO_POS_1_OR_2_DIST = 9;
-        private static int STRAFE_TO_POS_3_DIST = 3;
+        private static int STRAFE_TO_POS_1_OR_2_DIST = 4;
+        private static int STRAFE_TO_POS_3_DIST = 4;
         private static int MOVE_FORWARD_TO_GET_1ST_STONE_DIST = 27;
         private static int MOVE_BACKWARD_AFTER_GRAB_DIST = 12;
         private static int STRAFE_UNDER_BRIDGE_1ST_STONE_DIST = 48;
-        private static int STRAFE_BACK_TO_GET_STONE_45_DIST = 72;
-        private static int MOVE_FORWARD_TO_GET_STONE_45_DIST = 13;
-        private static int MOVE_FORWARD_TO_GET_STONE_6_DIST = 11;
+        private static int STRAFE_BACK_TO_GET_2ND_STONE_DIST = 72;
+        private static int MOVE_FORWARD_TO_GET_2ND_STONE_DIST = 13;
         private static int STRAFE_UNDER_BRIDGE_2ND_STONE_DIST = 73;
         private static int STRAFE_TO_PARK_DIST = 20;
 
@@ -37,22 +35,21 @@ public class DoubleSkystone extends BaseLinearOpMode
         @Override
         public void runOpMode()
         {
-            // using park on side to change this for now
-            // but eventually we will use the value returned from findSkystonePosition();
-
             initialize(true);
             configMode();
 
             telemetry.addData(">", "Press Play to start op mode");
             telemetry.update();
+
             waitForStart();
 
             if (opModeIsActive()) {
                 if (position == 3) {
                     driveTo3();
                     lowerHook();
-                    STRAFE_UNDER_BRIDGE_1ST_STONE_DIST += 6;
+                    STRAFE_UNDER_BRIDGE_1ST_STONE_DIST += 8;
                     deposit123();
+                    STRAFE_BACK_TO_GET_2ND_STONE_DIST += 8;
                     driveTo456();
                     STRAFE_UNDER_BRIDGE_2ND_STONE_DIST += 8;
                     deposit456();
@@ -64,20 +61,18 @@ public class DoubleSkystone extends BaseLinearOpMode
                     lowerHook();
                     deposit123();
                     driveTo456();
+                    lowerHook();
                     deposit456();
                     park();
-                    // make compatible with blue
-
                 }
             }
-
         }
 
 
 
         public void lowerHook()
         {
-            if (isStartingBlue) {
+            if (isBlue) {
                 if (position == 1)
                     robot.hooks.lowerOneHook("R");
                 else
@@ -89,15 +84,16 @@ public class DoubleSkystone extends BaseLinearOpMode
                     robot.hooks.lowerOneHook("R");
             }
         }
+
         // EVERYTHING IS FLIPPED BECAUSE ROBOT IS BACKWARDS
         public void driveTo1Or2()
         {
-            if(isStartingBlue)
+            if(isBlue)
             {
                 moveBackwardByInches(0.5, MOVE_OFF_OF_WALL_DISTANCE);
                 strafeRightByInches(0.85, STRAFE_TO_POS_1_OR_2_DIST);
                 moveBackwardByInches(1, MOVE_FORWARD_TO_GET_1ST_STONE_DIST);
-            } else{
+            } else {
                 moveBackwardByInches(0.5, MOVE_OFF_OF_WALL_DISTANCE);
                 strafeLeftByInches(0.85, STRAFE_TO_POS_1_OR_2_DIST);
                 moveBackwardByInches(1, MOVE_FORWARD_TO_GET_1ST_STONE_DIST);
@@ -109,11 +105,11 @@ public class DoubleSkystone extends BaseLinearOpMode
         {
             moveBackwardByInches(0.5, MOVE_OFF_OF_WALL_DISTANCE);
 
-            if(isStartingBlue)
+            if(isBlue)
             {
-                strafeRightByInches(0.85, STRAFE_TO_POS_3_DIST);
-            } else {
                 strafeLeftByInches(0.85, STRAFE_TO_POS_3_DIST);
+            } else {
+                strafeRightByInches(0.85, STRAFE_TO_POS_3_DIST);
             }
             moveBackwardByInches(1, MOVE_FORWARD_TO_GET_1ST_STONE_DIST);
         }
@@ -122,27 +118,15 @@ public class DoubleSkystone extends BaseLinearOpMode
 
         public void driveTo456 ()
         {
-            if(isStartingBlue) {
-                if (position == 3) {
-                    strafeLeftByInches(1, STRAFE_BACK_TO_GET_STONE_45_DIST + 8);
-                    moveBackwardByInches(1, MOVE_FORWARD_TO_GET_STONE_6_DIST);
-                } else {
-                    strafeLeftByInches(1, STRAFE_BACK_TO_GET_STONE_45_DIST);
-                    moveBackwardByInches(1, MOVE_FORWARD_TO_GET_STONE_45_DIST);
-                }
+            if(isBlue) {
+                strafeLeftByInches(1, STRAFE_BACK_TO_GET_2ND_STONE_DIST);
             }
             else {
-                if(position == 3) {
-                    strafeRightByInches(1, STRAFE_BACK_TO_GET_STONE_45_DIST + 8);
-                    moveBackwardByInches(1, MOVE_FORWARD_TO_GET_STONE_6_DIST);
-                }
-                else {
-                    strafeRightByInches(1, STRAFE_BACK_TO_GET_STONE_45_DIST);
-                    moveBackwardByInches(1, MOVE_FORWARD_TO_GET_STONE_45_DIST);
-                }
-            }
+                strafeRightByInches(1, STRAFE_BACK_TO_GET_2ND_STONE_DIST);
 
-            lowerHook();
+            }
+            moveBackwardByInches(1, MOVE_FORWARD_TO_GET_2ND_STONE_DIST);
+
         }
 
         // EVERYTHING IS FLIPPED BECAUSE ROBOT IS BACKWARDS!
@@ -150,7 +134,7 @@ public class DoubleSkystone extends BaseLinearOpMode
         {
             moveForwardByInches(0.85, MOVE_BACKWARD_AFTER_GRAB_DIST);
 
-            if (isStartingBlue)
+            if (isBlue)
                 strafeRightByInches(1, STRAFE_UNDER_BRIDGE_1ST_STONE_DIST);
             else
                 strafeLeftByInches(1, STRAFE_UNDER_BRIDGE_1ST_STONE_DIST);
@@ -162,7 +146,7 @@ public class DoubleSkystone extends BaseLinearOpMode
         {
             moveForwardByInches(0.85, MOVE_BACKWARD_AFTER_GRAB_DIST);
 
-            if (isStartingBlue)
+            if (isBlue)
                 strafeRightByInches(1, STRAFE_UNDER_BRIDGE_2ND_STONE_DIST);
             else
                 strafeLeftByInches(1, STRAFE_UNDER_BRIDGE_2ND_STONE_DIST);
@@ -171,10 +155,13 @@ public class DoubleSkystone extends BaseLinearOpMode
         }
 
         public void park() {
-            if (isStartingBlue)
+            if (isBlue) {
                 strafeLeftByInches(1, STRAFE_TO_PARK_DIST);
-            else
+            }
+            else {
                 strafeRightByInches(1, STRAFE_TO_PARK_DIST);
+            }
+            moveBackwardByInches(0.5, 6);
         }
 
         public void runOpMode2()
@@ -191,14 +178,14 @@ public class DoubleSkystone extends BaseLinearOpMode
                 moveForwardByInches(0.6, 14);
 
                 // IF IT'S THE FIRST STONE
-                if (isSkystone(isStartingBlue)) {
+                if (isSkystone(isBlue)) {
                     moveClampOutInAuto();
-                    grabAndTurn(isStartingBlue);
+                    grabAndTurn(isBlue);
                     moveForwardByInches(1, 30);
                     dropStone();
 
                     // return to get the 4th stone
-                    getNextStone(isStartingBlue, 4);
+                    getNextStone(isBlue, 4);
                     moveForwardByInches(1, 54);
                     dropStone();
 
@@ -208,16 +195,16 @@ public class DoubleSkystone extends BaseLinearOpMode
                 else
                 {
                     // goes to the next stone
-                    strafeToStone(isStartingBlue);
+                    strafeToStone(isBlue);
 
                     // IF IT'S THE SECOND STONE
-                    if(isSkystone(isStartingBlue)) {
+                    if(isSkystone(isBlue)) {
                         moveClampOutInAuto();
-                        grabAndTurn(isStartingBlue);
+                        grabAndTurn(isBlue);
                         moveForwardByInches(1, 38);
                         dropStone();
 
-                        getNextStone(isStartingBlue, 5);
+                        getNextStone(isBlue, 5);
                         moveForwardByInches(1, 62);
                         dropStone();
 
@@ -226,13 +213,13 @@ public class DoubleSkystone extends BaseLinearOpMode
                     else
                     {
                         // goes to the next stone
-                        strafeToStone(isStartingBlue);
+                        strafeToStone(isBlue);
                         moveClampOutInAuto();
-                        grabAndTurn(isStartingBlue);
+                        grabAndTurn(isBlue);
                         moveForwardByInches(1, 46);
 
                         // Can we get the 6th stone???
-                        getNextStone(isStartingBlue, 6);
+                        getNextStone(isBlue, 6);
                         moveForwardByInches(1, 70);
                         dropStone();
 
@@ -248,7 +235,7 @@ public class DoubleSkystone extends BaseLinearOpMode
             telemetry.update();
             do {
                 if (VenomUtilities.isValueChangedAndEqualTo("1.y", gamepad1.y, true))
-                    isStartingBlue = !isStartingBlue;
+                    isBlue = !isBlue;
 
                 if (VenomUtilities.isValueChangedAndEqualTo("1.a", gamepad1.a, true))
                     parkOnSide = !parkOnSide;
@@ -267,11 +254,11 @@ public class DoubleSkystone extends BaseLinearOpMode
         void logConfigModes(boolean update) {
             String modes="";
             //  modes+="Alliance="+(isRedAlliance?"Red":"Blue");
-            modes+=", Starting="+(isStartingBlue?"Blue":"Red");
+            modes+=", Starting="+(isBlue ?"Blue":"Red");
             modes+=", Starting="+(parkOnSide?"Side":"Center");
 
 
-            telemetry.addData("Alliance (Y)", isStartingBlue?"Blue":"Red");
+            telemetry.addData("Alliance (Y)", isBlue ?"Blue":"Red");
             telemetry.addData("Park Location (A)", parkOnSide?"Side":"Center");
 
             if (configOnly) telemetry.addData("ConfigMode" , "Press right bumper to leave config mode.");
@@ -285,7 +272,7 @@ public class DoubleSkystone extends BaseLinearOpMode
 
         public void parkOnlySkystone() {
 
-            if (isStartingBlue) {
+            if (isBlue) {
                 strafeRightByInches(0.7, 3);
             } else {
                 strafeLeftByInches(0.7, 3);
@@ -329,6 +316,22 @@ public class DoubleSkystone extends BaseLinearOpMode
                 strafeLeftByInches(1, 32);
 
             grabAndTurn(false);
+        }
+    }
+
+    @Override
+    public synchronized void waitForStart() {
+        while (!isStarted()) {
+            synchronized (this) {
+                try {
+                    StonePosition sp = getSkystonePosition();
+                    position = sp.getPosition(isBlue);
+                    this.wait(50);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StonePosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StonePosition.java
@@ -4,19 +4,24 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Color;
 
 public enum StonePosition {
-    Left(BaseLinearOpMode.MIDDLE_OF_LEFT_BLOCK_TO_LEFT_EDGE, Color.RED),
-    Middle(BaseLinearOpMode.MIDDLE_OF_MIDDLE_BLOCK_TO_LEFT_EDGE, Color.BLUE),
-    Right(BaseLinearOpMode.MIDDLE_OF_RIGHT_BLOCK_TO_LEFT_EDGE, Color.GREEN);
+    Left(BaseLinearOpMode.MIDDLE_OF_LEFT_BLOCK_TO_LEFT_EDGE, Color.RED, 1, 3),
+    Middle(BaseLinearOpMode.MIDDLE_OF_MIDDLE_BLOCK_TO_LEFT_EDGE, Color.BLUE, 2, 2),
+    Right(BaseLinearOpMode.MIDDLE_OF_RIGHT_BLOCK_TO_LEFT_EDGE, Color.GREEN, 3, 1);
 
 
     private final int centerX;
     private final int centerY;
     private final int color;
+    private final int bluePos;
+    private final int redPos;
 
-    StonePosition(int centerX, int color) {
+
+    StonePosition(int centerX, int color, int bluePos, int redPos) {
         this.centerX = centerX;
         this.centerY = BaseLinearOpMode.MIDDLE_OF_BLOCK_TO_TOP_OF_IMAGE;
         this.color = color;
+        this.bluePos = bluePos;
+        this.redPos = redPos;
     }
 
     public int getCenterX() {
@@ -31,4 +36,12 @@ public enum StonePosition {
         return color;
     }
 
+    public int getPosition(boolean isBlue){
+        if(isBlue){
+            return bluePos;
+        }
+        else{
+            return redPos;
+        }
+    }
 }
